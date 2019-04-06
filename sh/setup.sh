dd8c1e6ce17f147d7273d4ccc97f17e8ec8d61cd
boxmein <wsdf294@gmail.com>
2019-04-07 00:49:53 +0300

    Auto install packages

--- sh/setup.sh
+++ sh/setup.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
-set -e
+set -el
+
+# Set up Raspbian for production mode
+
+IS_RASPBIAN=$([[ $(lsb_release -a | grep "Distributor") == *Raspbian* ]])
+INSTALL_AUTOMATICALLY=${INSTALL_AUTOMATICALLY:-$IS_RASPBIAN}
+
+if [[ $IS_RASPBIAN ]]; then
+  echo "Raspbian OS detected"
+fi
+
+if [[ $INSTALL_AUTOMATICALLY ]]; then
+  echo "Installing dependencies without prompting."
+  echo "You may be asked for sudo password."
+fi
 
 if ! command -v pipenv >/dev/null 2>/dev/null; then
-  echo "pipenv not installed!"
-  echo "install pipenv:"
-  echo "python3 -mpip install pipenv"
-  exit 1
+  if [[ $INSTALL_AUTOMATICALLY ]]; then
+    sudo apt-get -y install python3 python3-pip
+    sudo pip3 install pipenv
+  else
+    echo "pipenv not installed!"
+    echo "install pipenv:"
+    echo "python3 -mpip install pipenv"
+    exit 1
+  fi
 fi
 
 if ! command -v node >/dev/null 2>/dev/null; then
-  echo "node.js not installed!"
-  echo "install nodejs: https://nodejs.org"
-  exit 1
+  if [[ $INSTALL_AUTOMATICALLY ]]; then
+    curl -L https://git.io/n-install | bash
+    sudo ~/n/bin/n 10.15.3
+  else
+    echo "node.js not installed!"
+    echo "install nodejs: https://nodejs.org"
+    exit 1
+  fi
 fi
 
 if ! command -v yarn >/dev/null 2>/dev/null; then
-  echo "yarn not installed!"
-  echo "install yarn: https://yarnpkg.com"
-  echo "or npm install -g yarn"
-  exit 1
+  if [[ $INSTALL_AUTOMATICALLY ]]; then
+    sudo npm install -g yarn
+  else
+    echo "yarn not installed!"
+    echo "install yarn: https://yarnpkg.com"
+    echo "or npm install -g yarn"
+    exit 1
+  fi
 fi
 
-## Set up Node.js project
-
-cd frontend
-yarn
-
-echo "setup done! start with sh/start.sh"
 
