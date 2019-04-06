e2e29841388ba379d47495fa6d97bda205ecfc50
boxmein <wsdf294@gmail.com>
2019-04-07 02:31:22 +0300

    Try and improve Dockerfile by making the script copy files right

---
+++ sh/build-docker-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Builds the Docker image used for deployment
+
+
+# Ensure we are at the top folder of the project
+if command -v git >/dev/null 2>/dev/null; then
+  cd $(git rev-parse --show-toplevel)
+else
+  echo "NOTE: make sure this command is run in the top folder of the project!"
+fi
+
+exec docker build -t kfst-boat-visualizer .
