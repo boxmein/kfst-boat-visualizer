f3b2fb068dc8293a5d0533ef041d223eb1036d00
boxmein <wsdf294@gmail.com>
2019-04-06 03:10:24 +0300

    Add a clock thread that goes "bing"

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from threading import Thread
+import time
 
 from flask import Flask
 from flask_socketio import SocketIO
@@ -23,7 +24,7 @@ def indexpage():
 
 def serial_listener(sio):
     id_ticker = 0
-    def tick(message_type, packet_raw_data, additional_data={}):
+    def tick(message_type, packet_raw_data, parsed={}):
         nonlocal id_ticker
         print("+ serial message received")
         id_ticker += 1
@@ -32,7 +33,7 @@ def serial_listener(sio):
             '_id': id_ticker,
             'type': message_type,
             'raw_data': packet_raw_data.hex(),
-            'contents': additional_data
+            'parsed': parsed
         }
 
         sio.emit('message', message)
@@ -40,16 +41,33 @@ def serial_listener(sio):
     print("+ subscribing to serial")
     reader(tick)
 
+def clock(sio):
+    id_ticker = 0
+    
+    while True:
+        print('+ emitting clock')
+        id_ticker += 1
+        message = {
+            '_id': id_ticker,
+            'type': 'ping'
+        }
+        sio.emit('message', message)
+        time.sleep(1)
+
 
 serial_thread = Thread(target=serial_listener, args=(sio,))
 serial_thread.daemon = True
+
+clock_thread = Thread(target=clock, args=(sio,))
+clock_thread.daemon = True
 
 ##
 ## Entry point
 ##
 
 if __name__ == '__main__':
-    print("+ starting bg task")
+    print("+ starting bg tasks")
+    clock_thread.start()
     serial_thread.start()
     print("+ starting app")
     sio.run(app)
