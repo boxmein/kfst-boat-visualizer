47b9693337a4c3dead30afee00bdb1f3c1caf08f
boxmein <wsdf294@gmail.com>
2019-05-26 14:23:36 +0300

    Delete pipenv from sh/start only backend

--- sh/start-only-backend.sh
+++ sh/start-only-backend.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
-if ! command -v pipenv >/dev/null 2>/dev/null; then
-  echo "pipenv not found"
-  echo "install it with python3 -mpip install --user pipenv"
-  echo "note: if you don't have pip, install it with sudo apt install python3-pip on Raspbian"
-  exit 1
-fi
+#if ! command -v pipenv >/dev/null 2>/dev/null; then
+#  echo "pipenv not found"
+#  echo "install it with python3 -mpip install --user pipenv"
+#  echo "note: if you don't have pip, install it with sudo apt install python3-pip on Raspbian"
+#  exit 1
+#fi
 
 # clean up before running
 rm -f server.log
 
-exec pipenv run python3 server/main.py $*
+# install deps
+# pip3 install -r requirements.txt
+
+exec python3 server/main.py $*
